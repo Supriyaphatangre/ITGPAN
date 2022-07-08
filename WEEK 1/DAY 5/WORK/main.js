b64cc06c3079f9968256e6bf56721121f46909f8
supriyaphatangre <supriyaphatangre7@gmail.com>
2022-07-08 16:43:18 +0530

    add file

---
+++ WEEK 1/DAY 5/WORK/main.js
@@ -0,0 +1,15 @@
+let username;
+let password;
+
+    function login(){
+        username = document.getElementById("username").Value;
+        password = document.getElementById("password").Value;
+
+        if((username="supriya") && (password="777")){
+        alert("login successful")
+    }
+    else{
+        alert("login unsuccessful")
+    }
+}
+
