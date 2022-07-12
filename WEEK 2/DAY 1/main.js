42c476960b5ec13cd0b1d210543fec8b1b80f2be
supriyaphatangre <supriyaphatangre7@gmail.com>
2022-07-12 14:01:46 +0530

    file added successfully

---
+++ WEEK 2/DAY 1/main.js
@@ -0,0 +1,35 @@
+let email;
+let password;
+let student_name;
+let clg_name;
+let branch;
+let phone_no;
+let address;
+
+function reg(){
+    email = document.getElementById("Email").value;
+    password = document.getElementById("Password").value;
+    student_name = document.getElementById("Student_name").value;
+    clg_name = document.getElementById("Clg_name").value;
+    branch= document.getElementById("Branch").value;
+    phone_no = document.getElementById("Phone_no").value;
+    address = document.getElementById("Address").value;
+    
+    document.write("<br/> Email="+email)
+    document.write("<br/> Password="+password)
+    document.write("<br/> Student_name="+student_name)
+    document.write("<br/> Clg_name="+clg_name)
+    document.write("<br/> Branch="+branch)
+    document.write("<br/> Phone_no="+phone_no)
+    document.write("<br/> Address="+address)
+
+
+
+    
+
+
+
+
+
+}
+
