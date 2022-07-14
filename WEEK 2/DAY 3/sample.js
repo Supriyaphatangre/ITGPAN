4c8f243b9f49c11cbbac7815d24ef9ff6bfe353a
supriyaphatangre <supriyaphatangre7@gmail.com>
2022-07-14 10:30:11 +0530

    file added successfully

---
+++ WEEK 2/DAY 3/sample.js
@@ -0,0 +1,23 @@
+// let row = 1;
+// let entry=document.getElementById("entry");
+// entry.addEventListener("click",displayDetails);
+
+
+// function add_data(){
+//     let name = document.getElementById("name").Value;
+//     let roll = document.getElementById("roll").value;
+//     let email = document.getElementById("email").value;
+
+
+// let display=document.getElementById("display");
+
+// let newRow=display.insertRow(row);
+// let cell1=newRow.insertCell(0);
+// let cell2=newRow.insertCell(1);
+// let cell3=newRow.insertCell(2);
+
+// cell1.innerHTML=name;
+// cell2.innerHTML=roll;
+// cell3.innerHTML=email;
+// row++;
+// }
